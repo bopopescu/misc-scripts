083a247633260314a44ee4853cd19d5e8278fabe
twitchyliquid64 <none>
2016-07-10 12:07:47 +0000

    pinode button daemon now actually works

--- pinode_daemon.py
+++ pinode_daemon.py
@@ -11,18 +11,31 @@ but_led_right = 18
 but_right = 22
 but_left = 27
 
+GPIO.setup(but_right, GPIO.IN)
+GPIO.setup(but_left, GPIO.IN)
+
 rdy_led = 19
 flt_led = 26
 
 beacon_led = 13
 blue_led = 6
 
+time_stamp = time.time()
+
+def bounce():
+    global time_stamp
+    time_now = time.time()  
+    if (time_now - time_stamp) >= 0.3:
+        time_stamp = time.time()
+        return False
+    return True
 
 def onLeftButton(channel):
-    pass
+    if not bounce():
+        print 'left'
 
 def onRightButton(channel):
-    pass
+    print 'right'
 
 
 GPIO.add_event_detect(but_right, GPIO.RISING, callback=onRightButton)
@@ -31,6 +44,8 @@ GPIO.add_event_detect(but_left, GPIO.RISING, callback=onLeftButton)
 try:
     while True:
         time.sleep(4)
+except:
+    pass
 finally:
     GPIO.cleanup()
 
